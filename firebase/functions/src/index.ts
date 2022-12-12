1c35e0dbe2973e29fc84bf6806fceb3a92176213
Gabriel <ilie.cgabriel@gmail.com>
2022-12-12 21:54:31 +0200

    got jwt in firebase function

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,5 +1,12 @@
 import * as functions from "firebase-functions";
 
 export const addGoal = functions.https.onCall((request, context) => {
-  return {message: '', code: 200};
+
+  const authorizationHeader = context.rawRequest.headers.authorization as String;
+  if(authorizationHeader){
+    const token = authorizationHeader.split(' ' )[1];
+    console.log(token);
+  }
+
+  return {message: "", code: 200};
 });
