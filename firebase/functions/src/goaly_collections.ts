91af67cdb7721c5319498ed0698126e61d0bd5bc
Gabriel <ilie.cgabriel@gmail.com>
2023-01-03 20:06:14 +0200

    added GoalyCollections

---
+++ firebase/functions/src/goaly_collections.ts
@@ -0,0 +1,11 @@
+import * as admin from 'firebase-admin'
+
+export class GoalyCollections {
+    static get users() {
+        return admin.firestore().collection('users');
+    }
+
+    static goalsOfUser(userUid: string) {
+        return admin.firestore().collection('users').doc(userUid).collection('goals');
+    }
+}
---
+++ firebase/functions/src/services/googleOauthClientFactory.ts
@@ -0,0 +1,10 @@
+import {auth} from "@googleapis/calendar";
+
+export class GoogleOauthClientFactory {
+  static createFromToken(googleAccessToken: string) {
+    const authClient = new auth.OAuth2();
+    authClient.setCredentials({access_token: googleAccessToken});
+
+    return authClient;
+  }
+}
