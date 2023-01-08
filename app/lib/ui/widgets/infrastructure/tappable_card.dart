17c29829ce4d28e707ecf700f358fa7346f9ba16
Gabriel <ilie.cgabriel@gmail.com>
2023-01-08 17:36:49 +0200

    cleanup for TappableCard

--- app/lib/ui/widgets/infrastructure/tappable_card.dart
+++ app/lib/ui/widgets/infrastructure/tappable_card.dart
@@ -3,14 +3,15 @@ import 'package:flutter/material.dart';
 class TappableCard extends StatelessWidget {
   final Widget child;
   final double height;
-  final double width;
+  final double? width;
   final void Function()? onTap;
+  final borderRadius = 12.0;
 
   const TappableCard({
     required this.child,
     this.onTap,
     required this.height,
-    required this.width,
+    this.width,
     Key? key,
   }) : super(key: key);
 
@@ -18,10 +19,10 @@ class TappableCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(borderRadius),
       ),
       child: InkWell(
-        borderRadius: BorderRadius.circular(12),
+        borderRadius: BorderRadius.circular(borderRadius),
         onTap: onTap,
         child: Container(
           margin: EdgeInsets.zero,
