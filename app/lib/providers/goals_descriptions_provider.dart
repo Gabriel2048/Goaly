b18c527131973f47a139e482602917e5808e2f2a
Gabriel <ilie.cgabriel@gmail.com>
2023-01-09 19:44:54 +0200

    restructured goal description provider

--- app/lib/providers/goals_descriptions_provider.dart
+++ app/lib/providers/goals_descriptions_provider.dart
@@ -1,18 +1,22 @@
+import 'dart:collection';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:goaly/domain/goal_type.dart';
 
-final goalDescriptionProvider = Provider<Map<GoalType, GoalDescription>>((ref) {
-  return const {
-    GoalType.gym:
-        GoalDescription('assets/goals/icons/gym.png', 'Go to the gym'),
-    GoalType.custom:
-        GoalDescription('assets/goals/icons/gym.png', 'Custom goal')
-  };
+final goalDescriptionProvider =
+    Provider<UnmodifiableListView<GoalDescription>>((ref) {
+  return UnmodifiableListView(const [
+    GoalDescription(
+        'assets/goals/icons/gym.png', 'Go to the gym', GoalType.gym),
+    GoalDescription(
+        'assets/goals/icons/gym.png', 'Custom goal', GoalType.custom)
+  ]);
 });
 
 class GoalDescription {
+  final GoalType goalType;
   final String assetPath;
   final String description;
 
-  const GoalDescription(this.assetPath, this.description);
+  const GoalDescription(this.assetPath, this.description, this.goalType);
 }
