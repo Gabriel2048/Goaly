68ceaca0b2d90aa4323b3d803a409bf673308dd8
Gabriel <ilie.cgabriel@gmail.com>
2023-01-16 18:20:01 +0200

    small cleanup for GoogleCalendarService

--- firebase/functions/src/services/googleCalendarService.ts
+++ firebase/functions/src/services/googleCalendarService.ts
@@ -2,13 +2,15 @@ import { OAuth2Client } from "google-auth-library";
 import { calendar } from "@googleapis/calendar";
 import { AddedEvent, EventToAdd } from "../types/google_calendar_types";
 
+const calendarClient = calendar("v3");
+
 export class GoogleCalendarService {
+    private readonly calendarId = 'primary';
     constructor(private auth: OAuth2Client) { }
 
     async addEvent(eventToAdd: EventToAdd): Promise<AddedEvent> {
-        const calendarClient = calendar("v3");
         return await calendarClient.events.insert({
-            calendarId: "primary",
+            calendarId: this.calendarId,
             auth: this.auth,
             requestBody: eventToAdd,
         });
