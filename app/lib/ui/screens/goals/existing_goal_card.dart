5b31409510661506ae318c55203859913e7d16ba
Gabriel <ilie.cgabriel@gmail.com>
2023-02-01 19:20:52 +0200

    adjusted clipping of expandable card

--- app/lib/ui/screens/goals/existing_goal_card.dart
+++ app/lib/ui/screens/goals/existing_goal_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:goaly/domain/goal.dart';
 import 'package:goaly/providers/goals_descriptions_provider.dart';
+import 'package:goaly/ui/screens/add_goal_details/frequency_dropdown.dart';
 import 'package:goaly/ui/widgets/infrastructure/tappable_card.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -25,41 +26,47 @@ class _ExistingGoalCardState extends State<ExistingGoalCard> {
           isExpanded = !isExpanded;
         });
       },
-      child: Consumer(
-        builder: (_, WidgetRef ref, Widget? child) {
-          final goalsDescriptions = ref.watch(goalDescriptionProvider);
-          final goal = goalsDescriptions.singleWhere(
-              (element) => element.goalType == widget.goal.goalType);
-          return Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        height: isExpanded ? 200 : 100,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 18.0),
+          child: Consumer(
+            builder: (_, WidgetRef ref, Widget? child) {
+              final goalsDescriptions = ref.watch(goalDescriptionProvider);
+              final goal = goalsDescriptions.singleWhere(
+                  (element) => element.goalType == widget.goal.goalType);
+              return Wrap(
+                clipBehavior: Clip.antiAlias,
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Text(
-                      goal.description,
-                      style: GoogleFonts.chewy(
-                        textStyle: Theme.of(context).textTheme.headlineSmall,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        goal.description,
+                        style: GoogleFonts.chewy(
+                          textStyle: Theme.of(context).textTheme.headlineSmall,
+                        ),
                       ),
+                      Image.asset(
+                        goal.assetPath,
+                        color: Colors.white.withOpacity(0.3),
+                        colorBlendMode: BlendMode.modulate,
+                        height: 100,
+                      ),
+                    ],
+                  ),
+                  Text(
+                    frequencyToLabelMap[widget.goal.frequency]!,
+                    style: GoogleFonts.chewy(
+                      textStyle: Theme.of(context).textTheme.bodyLarge,
                     ),
-                  ),
-                  Image.asset(
-                    goal.assetPath,
-                    color: Colors.white.withOpacity(0.3),
-                    colorBlendMode: BlendMode.modulate,
-                    height: 100,
-                  ),
+                  )
                 ],
-              ),
-              AnimatedContainer(
-                height: isExpanded ? 100 : 0,
-                duration: const Duration(milliseconds: 350),
-                child: const Text("more"),
-              )
-            ],
-          );
-        },
+              );
+            },
+          ),
+        ),
       ),
     );
   }
