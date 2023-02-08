6e7f66b662fb7314cb99ac4d29971aa61e4d81b7
Gabriel <ilie.cgabriel@gmail.com>
2023-02-08 21:31:14 +0200

    added logic for adding an event

--- app/lib/services/calendar/calendar_service.dart
+++ app/lib/services/calendar/calendar_service.dart
@@ -7,10 +7,22 @@ class CalendarService {
 
   CalendarService(this._googleAuthService);
 
-  Future<void> test() async {
-    final events = await _runWithCalendarClient((calendarClient) async {
-      return await calendarClient.events.list('primary');
-    });
+  Future<String> getUsersTimeZone() async {
+    final calendar = await _runWithCalendarClient(
+      (calendarClient) => calendarClient.calendars.get('primary'),
+    );
+    return calendar.timeZone!;
+  }
+
+  Future<void> addEvent(EventDateTime start, EventDateTime end) async {
+    final eventToAdd = Event(
+      summary: '[Goaly] My event',
+      start: start,
+      end: end,
+    );
+    await _runWithCalendarClient(
+      (calendarClient) => calendarClient.events.insert(eventToAdd, 'primary'),
+    );
   }
 
   /// Utility method for ensuring the AuthClient.close() is called.
