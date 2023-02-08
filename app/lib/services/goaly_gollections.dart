4012e7ef6efe99bb01f938e62b404f2e18404b28
Gabriel <ilie.cgabriel@gmail.com>
2023-02-08 21:49:22 +0200

    goals will now be added to firestore from the mobile app

--- app/lib/services/goaly_gollections.dart
+++ app/lib/services/goaly_gollections.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class GoalyCollections {
-  static CollectionReference<Map<String, dynamic>> goalsOfCurrentUser() {
+  static CollectionReference<Map<String, dynamic>> get goalsOfCurrentUser {
     return FirebaseFirestore.instance
         .collection('users')
         .doc(FirebaseAuth.instance.currentUser!.uid)
