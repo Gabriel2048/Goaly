a0861be420e32c8e0411718adb1094e10b0d7446
Gabriel <ilie.cgabriel@gmail.com>
2023-03-04 13:00:16 +0200

    added delete confirmation dialogue

--- app/lib/ui/screens/goals/existing_goal_card.dart
+++ app/lib/ui/screens/goals/existing_goal_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:goaly/domain/goal.dart';
 import 'package:goaly/main.dart';
 import 'package:goaly/providers/goals_descriptions_provider.dart';
+import 'package:goaly/services/goaly_gollections.dart';
 import 'package:goaly/ui/screens/add_goal_details/frequency_dropdown.dart';
 import 'package:goaly/ui/widgets/infrastructure/tappable_card.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -30,6 +31,37 @@ class _ExistingGoalCardState extends State<ExistingGoalCard> with RouteAware {
     );
   }
 
+  Future<bool> _handleConfirmDismiss(DismissDirection direction) async {
+    final canDelete = await showDialog<bool>(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: const Text("Deleting your goal"),
+        content: const Text(
+            "Your are about to delete this goal. This actions is irreversible.\n\nAre you sure?"),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop(true);
+            },
+            child: const Text("Yes, delete."),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop(false);
+            },
+            child: const Text("No, keep."),
+          ),
+        ],
+      ),
+    );
+
+    return canDelete ?? false;
+  }
+
+  Future<void> _handleDismissed(DismissDirection direction) async {
+    await GoalyCollections.goalsOfCurrentUser.doc(widget.goal.id).delete();
+  }
+
   @override
   void dispose() {
     routeObserver.unsubscribe(this);
@@ -45,72 +77,80 @@ class _ExistingGoalCardState extends State<ExistingGoalCard> with RouteAware {
 
   @override
   Widget build(BuildContext context) {
-    return TappableCard(
-      onTap: () {
-        setState(() {
-          isExpanded = !isExpanded;
-        });
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 250),
-        height: isExpanded ? 200 : 100,
-        child: Wrap(
-          runAlignment: WrapAlignment.spaceEvenly,
-          clipBehavior: Clip.antiAlias,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: padding),
-              child: Consumer(
-                builder: (BuildContext context, WidgetRef ref, Widget? child) {
-                  final goalsDescriptions = ref.watch(goalDescriptionProvider);
-                  final goalDescription = goalsDescriptions.singleWhere(
-                      (element) => element.goalType == widget.goal.goalType);
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        goalDescription.description,
-                        style: GoogleFonts.chewy(
-                          textStyle: Theme.of(context).textTheme.headlineSmall,
+    return Dismissible(
+      key: Key(widget.goal.id),
+      confirmDismiss: _handleConfirmDismiss,
+      onDismissed: _handleDismissed,
+      child: TappableCard(
+        onTap: () {
+          setState(() {
+            isExpanded = !isExpanded;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 250),
+          height: isExpanded ? 200 : 100,
+          child: Wrap(
+            runAlignment: WrapAlignment.spaceEvenly,
+            clipBehavior: Clip.antiAlias,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: padding),
+                child: Consumer(
+                  builder:
+                      (BuildContext context, WidgetRef ref, Widget? child) {
+                    final goalsDescriptions =
+                        ref.watch(goalDescriptionProvider);
+                    final goalDescription = goalsDescriptions.singleWhere(
+                        (element) => element.goalType == widget.goal.goalType);
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          goalDescription.description,
+                          style: GoogleFonts.chewy(
+                            textStyle:
+                                Theme.of(context).textTheme.headlineSmall,
+                          ),
                         ),
-                      ),
-                      Image.asset(
-                        goalDescription.assetPath,
-                        color: Colors.white.withOpacity(0.3),
-                        colorBlendMode: BlendMode.modulate,
-                        height: 100,
-                      ),
-                    ],
-                  );
-                },
+                        Image.asset(
+                          goalDescription.assetPath,
+                          color: Colors.white.withOpacity(0.3),
+                          colorBlendMode: BlendMode.modulate,
+                          height: 100,
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 18.0),
-              child: Divider(),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: padding),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    frequencyToLabelMap[widget.goal.frequency]!,
-                    style: GoogleFonts.chewy(
-                      textStyle: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 10.0),
-                    child: OutlinedButton(
-                      onPressed: () {},
-                      child: const Text('Check progress'),
-                    ),
-                  )
-                ],
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 18.0),
+                child: Divider(),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: padding),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      frequencyToLabelMap[widget.goal.frequency]!,
+                      style: GoogleFonts.chewy(
+                        textStyle: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 10.0),
+                      child: OutlinedButton(
+                        onPressed: () {},
+                        child: const Text('Check progress'),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
