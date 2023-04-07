fb3fb28d0d6f8ee27574e1287eb2dc7ab939d304
Gabriel <ilie.cgabriel@gmail.com>
2023-04-07 21:51:38 +0300

    Added time picker for goals that share same time

--- app/lib/ui/screens/add_goal_details/goal_forms/advanced/add_goal_details_advanced_form.dart
+++ app/lib/ui/screens/add_goal_details/goal_forms/advanced/add_goal_details_advanced_form.dart
@@ -13,6 +13,22 @@ class AddGoalDetailsAdvancedForm extends StatefulWidget {
 class _AddGoalDetailsAdvancedFormState
     extends State<AddGoalDetailsAdvancedForm> {
   var isSameHourEachDay = true;
+  TimeOfDay _time = const TimeOfDay(hour: 12, minute: 0);
+
+  void _onSameHourEachDayTap() {
+    setState(() {
+      isSameHourEachDay = !isSameHourEachDay;
+    });
+  }
+
+  void _onTimeTap() async {
+    final time = await showTimePicker(context: context, initialTime: _time);
+    if (time != null) {
+      setState(() {
+        _time = time;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,11 +41,24 @@ class _AddGoalDetailsAdvancedFormState
         SwitchListTileWithNoRipple(
           value: isSameHourEachDay,
           label: const Text("Same hour each day"),
-          onTap: () {
-            setState(() {
-              isSameHourEachDay = !isSameHourEachDay;
-            });
-          },
+          onTap: _onSameHourEachDayTap,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const Text("Select the time for the activity"),
+            Expanded(
+              child: GestureDetector(
+                onTap: _onTimeTap,
+                child: Center(
+                  child: Text(
+                    _time.format(context),
+                    style: const TextStyle(fontSize: 50),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
