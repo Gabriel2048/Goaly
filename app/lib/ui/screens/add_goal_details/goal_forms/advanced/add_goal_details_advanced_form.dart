d8218b0c8c3a9524c864af0fb2639f1d7e27a21b
Gabriel <ilie.cgabriel@gmail.com>
2023-05-17 21:12:32 +0300

    added title to advanced form

--- app/lib/ui/screens/add_goal_details/goal_forms/advanced/add_goal_details_advanced_form.dart
+++ app/lib/ui/screens/add_goal_details/goal_forms/advanced/add_goal_details_advanced_form.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:goaly/ui/screens/add_goal_details/goal_forms/advanced/day_picker.dart';
 import 'package:goaly/ui/screens/add_goal_details/goal_forms/advanced/time_form/frequency_form.dart';
+import 'package:goaly/ui/screens/add_goal_details/goal_forms/providers/goal_form_provider.dart';
+import 'package:provider/provider.dart';
 
 class AddGoalDetailsAdvancedForm extends StatelessWidget {
   const AddGoalDetailsAdvancedForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    final hasTitleConfigurable =
+        Provider.of<GoalFormProvider>(context, listen: false)
+            .hasTitleConfigurable;
+
+    return Column(
       children: [
-        DayPicker(),
-        SizedBox(
-          height: 20,
+        if (hasTitleConfigurable)
+          TextFormField(
+            onSaved: (String? value) {},
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Title',
+            ),
+          ),
+        if (hasTitleConfigurable)
+          const SizedBox(
+            height: 40,
+          ),
+        const DayPicker(),
+        const SizedBox(
+          height: 40,
         ),
-        TimeForm()
+        const TimeForm()
       ],
     );
   }
