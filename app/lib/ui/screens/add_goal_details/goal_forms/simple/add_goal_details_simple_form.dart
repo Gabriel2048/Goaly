d55dccd909077bf22f0c2217d1c923fde5149f85
Gabriel <ilie.cgabriel@gmail.com>
2023-05-21 13:37:51 +0300

    Fixed simple for spacing

--- app/lib/ui/screens/add_goal_details/goal_forms/simple/add_goal_details_simple_form.dart
+++ app/lib/ui/screens/add_goal_details/goal_forms/simple/add_goal_details_simple_form.dart
@@ -28,55 +28,58 @@ class _AddGoalDetailsSimpleFormState extends State<AddGoalDetailsSimpleForm> {
     const spacing = SizedBox(height: 40);
     return Form(
       key: _formKey,
-      child: Column(
-        children: [
-          if (hasTitleConfigurable)
-            TextFormField(
-              onSaved: (String? value) {
-                _formData[_titleKey] = value;
-              },
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Title',
+      child: Padding(
+        padding: const EdgeInsets.only(top: 5),
+        child: Column(
+          children: [
+            if (hasTitleConfigurable)
+              TextFormField(
+                onSaved: (String? value) {
+                  _formData[_titleKey] = value;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Title',
+                ),
               ),
+            if (hasTitleConfigurable) spacing,
+            FrequencyDropdown(
+              onSaved: (GoalFrequency? value) {
+                _formData[_frequencyKey] = value;
+              },
             ),
-          if (hasTitleConfigurable) spacing,
-          FrequencyDropdown(
-            onSaved: (GoalFrequency? value) {
-              _formData[_frequencyKey] = value;
-            },
-          ),
-          spacing,
-          TimeOfDayDropdown(
-            onSaved: (GoalTimeOfDay? value) {
-              _formData[_timeOfDayKey] = value;
-            },
-          ),
-          spacing,
-          spacing,
-          ElevatedButton.icon(
-            onPressed: () {
-              // final goalsService = ref.watch(goalsServiceProvider);
-              //
-              // if (_formKey.currentState?.validate() ?? false) {
-              //   _formKey.currentState?.save();
-              //   goalsService.addGoal(AddGoalModel(
-              //     title: widget.hasTitleConfigurable
-              //         ? _formData[_titleKey]
-              //         : null,
-              //   ));
-              // }
-            },
-            icon: const Padding(
-              padding: EdgeInsets.only(left: 20),
-              child: Icon(Icons.add),
+            spacing,
+            TimeOfDayDropdown(
+              onSaved: (GoalTimeOfDay? value) {
+                _formData[_timeOfDayKey] = value;
+              },
             ),
-            label: const Padding(
-              padding: EdgeInsets.only(top: 20, right: 20, bottom: 20),
-              child: Text('Add your goal'),
-            ),
-          )
-        ],
+            spacing,
+            spacing,
+            ElevatedButton.icon(
+              onPressed: () {
+                // final goalsService = ref.watch(goalsServiceProvider);
+                //
+                // if (_formKey.currentState?.validate() ?? false) {
+                //   _formKey.currentState?.save();
+                //   goalsService.addGoal(AddGoalModel(
+                //     title: widget.hasTitleConfigurable
+                //         ? _formData[_titleKey]
+                //         : null,
+                //   ));
+                // }
+              },
+              icon: const Padding(
+                padding: EdgeInsets.only(left: 20),
+                child: Icon(Icons.add),
+              ),
+              label: const Padding(
+                padding: EdgeInsets.only(top: 20, right: 20, bottom: 20),
+                child: Text('Add your goal'),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
